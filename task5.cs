45213de4e826a0ab844155599d192b1bc3fabcbc
Hanstheonly1 <157174833+Hanstheonly1@users.noreply.github.com>
2024-01-26 14:28:08 +0100

    Update task5.cs

--- task5.cs
+++ task5.cs
@@ -1,17 +1,32 @@
-// Your program should start at this line.
-/*
-Example:
-Move();
-Move();
-Move();
-Turn();
-Move();
-Move();
-Turn();
-Turn();
-Turn();
-Move();
-*/
+while(AtGoal = false)
+{
+    if(Intersection = true)
+    {
+        if(Peek_Left = true)
+        {
+            Turn_Left();
+            Move();
+        }
+        else
+        {
+            Move();
+        }
+    }
+    else if(Peek_1_Cell = false)
+    {
+        Turn_Right();
+        Move();
+    }
+    else if(Trail = true)
+    {
+        Turn_Right();
+        Move();
+    }
+    else
+    {
+        Move();
+    }
+}
 
 
 #region Basic functions
@@ -23,14 +38,37 @@ void Move()
     // Moves the car 1 cell in the direction it is heading. 
 }
 
-void Turn()
+void Turn_Right()
 {
     // Turns the car 90 deg clockwise.
 }
 
-bool Peek()
+void Turn_Left()
+{
+    // Turns the car 90 deg counter clockwise.
+}
+
+bool Trail()
+{
+    // Returns true if you turn to a cell you have already been on.
+    return true; // Just a placeholder value. 
+}
+
+bool Peek_1_Cell()
 {
     // Returns true if the next cell is open, otherwise false.
+    return true; // Just a placeholder value. 
+}
+
+bool Peek_Left()
+{
+    //Returns true if the cell to the left is open, otherwise false.
+    return true; // Just a placeholder value. 
+}
+
+bool Intersection()
+{
+    // Returns true if there are more than one open cell next to current cell.
     return true; // Just a placeholder value. 
 }
 
